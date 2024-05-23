d2a3f96df3c77ad99646253c3cd6ceab67873b0b
jeffigy <ejeffydev@gmail.com>
2024-05-23 22:11:19 +0800

    feat: configured Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LandingPage from "./pages/LandingPage";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path="/">
+        <Route index element={<LandingPage />} />
+
+        <Route path="home" element={<HomePage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
