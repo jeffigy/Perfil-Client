e8a0ed6a63b85489149ac486ae359b45fb293175
jeffigy <ejeffydev@gmail.com>
2024-05-25 22:36:36 +0800

    Added DaisyUI theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@ import daisyui from "daisyui";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  plugins: [daisyui],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  daisyui: {
+    themes: ["light", "dark", "night", "winter"],
+  },
 };
