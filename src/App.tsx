799434edc866c46bbf08d5c6afd55fac897d7ac9
jeffigy <ejeffydev@gmail.com>
2024-05-25 22:44:37 +0800

    feat: Added Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LandingPage from "./pages/LandingPage";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/">
+      <Route path="/" element={<Layout />}>
         <Route index element={<LandingPage />} />
 
         <Route path="home" element={<HomePage />} />
