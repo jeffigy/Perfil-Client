5993eac229d9d99168ec174b5e5d18155106b561
jeffigy <ejeffydev@gmail.com>
2024-07-25 22:41:53 +0800

    fix: Pushed update store file

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,11 +3,13 @@ import { apiSlice } from "./api/apiSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import authReducer from "features/auth/authSlice";
 import headerReducer from "features/common/headerSlice";
+import modalReducer from "features/common/modalSlice";
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
     header: headerReducer,
+    modal: modalReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
